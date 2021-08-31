aa99f76c3193f34775b93a18fa10cb1004a2a004
Bruno da Silva Valenga <brunodasilvalenga@gmail.com>
2021-08-31 15:06:13 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const playwright = require('playwright');
+
+const baseUrl = 'https://bmvs.onlineappointmentscheduling.net.au/oasis/Default.aspx';
+
+const search = {
+  city: 'Brisbane',
+  state: 'QLD'
+};
+
+(async () => {
+
+  const browser = await playwright.chromium.launch();
+  const page = await browser.newPage();
+
+  await page.goto(baseUrl);
+
+  await page.click('button.select-book-type-button');
+
+  await page.waitForLoadState('networkidle');
+
+  await page.fill('#ContentPlaceHolder1_SelectLocation1_txtSuburb', search.city);
+
+  await page.selectOption('select#ContentPlaceHolder1_SelectLocation1_ddlState', search.state);
+
+  await Promise.all([
+    page.click('input.blue-button'),
+    page.waitForNavigation(),
+  ]);
+
+  const locations = await page.$$("div#ContentPlaceHolder1_SelectLocation1_divLocations");
+  if (locations.length) {
+    console.log('Found locations!')
+    const foundLocations = await page.$$eval("table", (nodes) =>
+      nodes.map((n) => n.innerText)
+    );
+
+    console.log(foundLocations)
+  } else {
+    console.log('No locations Found :(')
+  }
+
+  await browser.close();
+
+})();
